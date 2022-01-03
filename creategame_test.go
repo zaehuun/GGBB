25f72d1913a65953f87ac018af5e7137576a1250
zaehuun <jdrjk0000@naver.com>
2022-01-03 21:00:43 +0900

    Test : 데이터 타입 및 함수 정의
    CreateGame 테스트 함수 내에서 사용되는 타입과 함수를
    정의했습니다. (Game,gameNumbers,CreateGame)

--- creategame_test.go
+++ creategame_test.go
@@ -2,7 +2,17 @@ package GGBB
 
 import "testing"
 
+type Game interface{
 
+}
+
+type gameNumbers struct {
+	nums string
+}
+
+func CreateGame(nums string) Game {
+	return &gameNumbers{nums: nums}
+}
 
 func TestCreateGame(t *testing.T){
 	var game Game = CreateGame("012")
