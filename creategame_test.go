e5fb01e0b66cf5e3e0a51bf3ca30d927e29edabe
zaehuun <jdrjk0000@naver.com>
2022-01-03 23:05:53 +0900

    Test : TDD 적용

--- creategame_test.go
+++ creategame_test.go
@@ -1,44 +1,12 @@
 package GGBB
 
 import (
-	"fmt"
-	"regexp"
 	"testing"
 )
 
-type Game interface{
 
-}
 
-type gameNumbers struct {
-	nums string
-}
 
-func CreateGame(nums string) (Game, error) {
-	//입력 값이 3~4 자리인지
-	var length  int = len(nums)
-	if length < 3 || length > 4 {
-		return nil, fmt.Errorf("invalued nums: %s", nums)
-	}
-
-	//입력 값이 숫자로만 이루어졌는지
-	matched, _ := regexp.MatchString("^[0-9]*$", nums)
-	if !matched{
-		return nil, fmt.Errorf("invalued nums: %s", nums)
-	}
-
-	//중복된 숫자가 있는지
-	used := make(map[uint8]bool)
-	for i := 0; i < length; i++ {
-		_, found := used[nums[i]]
-		fmt.Print(nums[i])
-		if found {
-			return nil, fmt.Errorf("invalued nums: %s", nums)
-		}
-		used[nums[i]] = true
-	}
-	return &gameNumbers{nums: nums}, nil
-}
 
 func TestInvalidNums(t *testing.T){
 
